ead82690e25b1dbb7ec15fbfa4edf829504a15a7
akash yadav <akashyadavmrt24@gmail.com>
2020-07-04 22:45:46 -0700

    Card commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react'
 import logo from './logo.svg';
+import { Navbar, NavbarBrand, Media} from 'reactstrap';
 import './App.css';
+import Menu from "./components/menucomponent";
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Navbar dark color="primary">
+      <div className="container">
+        <NavbarBrand href="/">Menu</NavbarBrand>
+      </div>
+    </Navbar>
+    <Menu />
+    </div>    
+       );
+
+  }
 }
 
 export default App;
